059ab923e39fea4a2fd983d12981f1bc1b23e5af
socratone <gim2origin@gmail.com>
2020-11-21 01:07:52 +0900

    각 button을 참조할 수 있도록 수정

--- src/PlayerPage.js
+++ src/PlayerPage.js
@@ -23,16 +23,22 @@ class PlayerPage extends Page {
   }
 
   appendPlayButtons() {
-    const tag = `
-      <div class="${styles.playButtons}">
-        <button class="repeat"><i class="fa fa-expand" aria-hidden="true"></i></button>
-        <button class="backward"><i class="fa fa-step-backward" aria-hidden="true"></i></i></button>
-        <button class="play"><i class="fa fa-play" aria-hidden="true"></i></button>
-        <button class="forward"><i class="fa fa-step-forward" aria-hidden="true"></i></button>
-        <button class="random"><i class="fa fa-random" aria-hidden="true"></i></button>
-      </div>
-    `;
-    this.element.insertAdjacentHTML('beforeend', tag)
+    const playButtons = document.createElement('div');
+    playButtons.classList.add(styles.playButtons);
+
+    this.repeat = document.createElement('button');
+    this.repeat.insertAdjacentHTML('beforeend', '<i class="fa fa-expand" aria-hidden="true"></i>');
+    this.backward = document.createElement('button');
+    this.backward.insertAdjacentHTML('beforeend', '<i class="fa fa-step-backward" aria-hidden="true"></i>');
+    this.play = document.createElement('button');
+    this.play.insertAdjacentHTML('beforeend', '<i class="fa fa-play" aria-hidden="true"></i>');
+    this.forward = document.createElement('button');
+    this.forward.insertAdjacentHTML('beforeend', '<i class="fa fa-step-forward" aria-hidden="true"></i>');
+    this.random = document.createElement('button');
+    this.random.insertAdjacentHTML('beforeend', '<i class="fa fa-random" aria-hidden="true"></i>');
+
+    playButtons.append(this.repeat, this.backward, this.play, this.forward, this.random);
+    this.element.append(playButtons);
   }
 }
 
