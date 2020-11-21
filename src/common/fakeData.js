06d7fd6af0b97dccaf814d873a85c7becf628e6c
socratone <gim2origin@gmail.com>
2020-11-22 01:48:22 +0900

    fakeData 수정 추가

--- src/common/fakeData.js
+++ src/common/fakeData.js
@@ -3,11 +3,12 @@ const videos = [
   { videoId: 'TgOu00Mf3kI', title: '아이유 - 에잇' },
   { videoId: '-5q5mZbe3V8', title: 'BTS (방탄소년단) &#39;Life Goes On&#39; Official MV' },
   { videoId: 'amnspvOH-EE', title: 'Blue &amp; Grey' },
-  { videoId: 'aJOTlE1K90k', title: 'Maroon 5 - Girls Like You ft. Cardi B (Official Music Video)' },
-  { videoId: 'SlPhMPnQ58k', title: 'Maroon 5 - Memories (Official Video)' },
+  { videoId: 'lDnz7oJv18E', title: 'Michael Jackson - Loving You (Audio)' },
+  { videoId: 'gdZLi9oWNZg', title: 'BTS (방탄소년단) \'Dynamite\' Official MV' },
   { videoId: '58hoktsqk_Q', title: 'Lady Gaga - 911 (Official Music Video)' },
   { videoId: 'JGPgxoIPY6Q', title: 'Boys Like Girls - The Great Escape (Official Video)' },
-  { videoId: 'ytLzxl4-mLw', title: 'Boys Like Girls - Thunder (Official Video)' },
+  { videoId: 'DC5Wmz6kjBs', title: 'Rhapsody - Dawn of Victory (Limb Music) [Full Album]' },
+  { videoId: 'h28fhU-mjDA', title: '장기하와 얼굴들 (Kiha & The Faces) - 그건 니 생각이고 (That’s Just What You Think) MV' },
   { videoId: 'fV4DiAyExN0', title: 'Hoobastank - The Reason (Official Music Video)' },
 ];
 
