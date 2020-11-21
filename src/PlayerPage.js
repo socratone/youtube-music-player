a90569b9513b8ec7e3081ae30e64010f38e2c0cf
socratone <gim2origin@gmail.com>
2020-11-21 20:28:52 +0900

    repeat 버튼 삭제

--- src/PlayerPage.js
+++ src/PlayerPage.js
@@ -68,15 +68,6 @@ class PlayerPage extends Page {
       this.playButtons.classList.add(styles.playButtons);
     };
 
-    // const setRepeatButton = () => {
-    //   this.repeat = document.createElement('button');
-    //   this.repeat.insertAdjacentHTML('beforeend', '<i class="fa fa-expand" aria-hidden="true"></i>');
-    //   this.repeat.addEventListener('click', () => {
-    //     console.log('repeat 버튼 클릭')
-    //   });
-    //   this.playButtons.append(this.repeat);
-    // };
-
     const setBackwardButton = () => {
       this.backward = document.createElement('button');
       this.backward.insertAdjacentHTML('beforeend', '<i class="fa fa-step-backward" aria-hidden="true"></i>');
@@ -158,7 +149,6 @@ class PlayerPage extends Page {
     };
 
     setButtonWrap();
-    // setRepeatButton();
     setBackwardButton();
     setPlayButton();
     setPauseButton();
