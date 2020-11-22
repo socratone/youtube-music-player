2667e09332dc715650e72be52ddbc92d619ec41f
socratone <gim2origin@gmail.com>
2020-11-23 08:07:41 +0900

    엔터 키로 검색 버튼 가능

--- src/page/SearchPage.js
+++ src/page/SearchPage.js
@@ -15,18 +15,9 @@ class SearchPage extends Page {
   }
 
   appendSearchComponent() {
-    this.input = document.createElement('input');
-    this.input.type = 'text';
-    // TODO: 엔터키 눌렀을 때
-   
-    const button = document.createElement('a');
-    button.innerHTML = '<i class="fa fa-search" aria-hidden="true"></i>';
-    
-    const buttonWrap = document.createElement('div');
-    buttonWrap.classList.add(styles.buttonWrap);
-    buttonWrap.append(button);
-    buttonWrap.addEventListener('click', async () => {
+    const handleButtonClick = async () => {
       const query = this.input.value;
+      if (query.length === 0) return alert('값을 입력하세요.');
       const data = await getVideosByQuery(query, 2);
       if (data.error) {
         return console.log(data.error.code, data.error.message);
@@ -35,7 +26,21 @@ class SearchPage extends Page {
       const filteredVideos = filterVideos(videos)
       console.log('filteredVideos:', filteredVideos)
       this.appendSearchResultList(filteredVideos);
+    };
+
+    this.input = document.createElement('input');
+    this.input.type = 'text';
+    this.input.addEventListener('keydown', ({ key }) => {
+      if (key === 'Enter') handleButtonClick();
     });
+   
+    const button = document.createElement('a');
+    button.innerHTML = '<i class="fa fa-search" aria-hidden="true"></i>';
+    
+    const buttonWrap = document.createElement('div');
+    buttonWrap.classList.add(styles.buttonWrap);
+    buttonWrap.append(button);
+    buttonWrap.addEventListener('click', handleButtonClick);
 
     const div = document.createElement('div');
     div.classList.add(styles.searchComponent);
