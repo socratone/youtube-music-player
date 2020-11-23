f711bd050c0177778729194ed46e67127161daaf
socratone <gim2origin@gmail.com>
2020-11-24 01:36:26 +0900

    unit test로 구분

--- src/common/__test__/Modal.test.js
+++ src/common/__test__/Modal.test.js
@@ -1,20 +1,22 @@
 import Modal from '../Modal.js';
 
-let modal;
-beforeEach(() => {
-  modal = new Modal('medium');
-});
-
-describe('setTitle 메소드', () => {
-  it('title이 정확히 들어가야 한다.', () => {
-    modal.setTitle('any');
-    expect(modal.title).toBe('any');
+describe('Unit Test', () => {
+  let modal;
+  beforeEach(() => {
+    modal = new Modal('medium');
   });
-});
   
-describe('setDescription 메소드', () => {
-  it('description이 정확히 들어가야 한다.', () => {
-    modal.setDescription('any');
-    expect(modal.description).toBe('any');
+  describe('setTitle 메소드', () => {
+    it('title이 정확히 들어가야 한다.', () => {
+      modal.setTitle('any');
+      expect(modal.title).toBe('any');
+    });
+  });
+    
+  describe('setDescription 메소드', () => {
+    it('description이 정확히 들어가야 한다.', () => {
+      modal.setDescription('any');
+      expect(modal.description).toBe('any');
+    });
   });
 });
