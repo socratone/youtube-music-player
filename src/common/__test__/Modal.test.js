8910ef33f746813a9ce87e8bd9907d54e160e8df
socratone <gim2origin@gmail.com>
2020-11-24 01:25:38 +0900

    Modal.test.js 생성

---
+++ src/common/__test__/Modal.test.js
@@ -0,0 +1,20 @@
+import Modal from '../Modal.js';
+
+let modal;
+beforeEach(() => {
+  modal = new Modal('medium');
+});
+
+describe('setTitle 메소드', () => {
+  it('title이 정확히 들어가야 한다.', () => {
+    modal.setTitle('any');
+    expect(modal.title).toBe('any');
+  });
+});
+  
+describe('setDescription 메소드', () => {
+  it('description이 정확히 들어가야 한다.', () => {
+    modal.setDescription('any');
+    expect(modal.description).toBe('any');
+  });
+});
