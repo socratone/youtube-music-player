e1ce33f7a4ce63210c1e8a11035aae58ca16c12d
socratone <gim2origin@gmail.com>
2020-11-23 09:16:47 +0900

    검색 설정 추가

--- src/helper/getVideosByQuery.js
+++ src/helper/getVideosByQuery.js
@@ -1,8 +1,16 @@
 import key from '../key/youtubeKey';
 
 const getVideosByQuery = (query, count = 10) => {
-  const url = 'https://www.googleapis.com/youtube/v3/search';
-  return fetch(`${url}?part=snippet&key=${key}&q=${query}&maxResults=${count}`)
+  let url = 'https://www.googleapis.com/youtube/v3/search';
+  url += '?part=snippet';
+  url += `&key=${key}`;
+  url += `&q=${query}`;
+  url += `&maxResults=${count}`;
+  url += '&type=video';
+  url += '&videoSyndicated=true'; // youtube.com 외부에서 재생할 수 있는 동영상
+  url += '&videoCategoryId=10'; // music
+
+  return fetch(url)
     .then(response => response.json())
     .catch(error => error);
 };
