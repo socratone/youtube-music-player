40b54f1e59cc6bc198ebeca7b49a090d03725a5e
socratone <gim2origin@gmail.com>
2020-11-24 18:15:23 +0900

    dropdown에 화살표 추가

--- src/common/Dropdown.js
+++ src/common/Dropdown.js
@@ -1,5 +1,8 @@
 import styles from './Dropdown.module.scss';
 
+const ARROW_HEIGHT = 8;
+const ARROW_LENGTH = 16;
+
 class Dropdown {
   constructor(size = 'medium') { // size: 'small' | 'medium' | 'large'
     this.size = size;
@@ -19,13 +22,17 @@ class Dropdown {
 
   setDirection(direction = 'down') { // direction: string
     this.direction = direction;
+    if (direction === 'left') this.arrowDirection = 'arrowRight';
+    else if (direction === 'right') this.arrowDirection = 'arrowLeft';
+    else if (direction === 'up') this.arrowDirection = 'arrowDown';
+    else this.arrowDirection = 'arrowUp';
   }
 
-  setGap(gap = 0) { // gap: number
+  setGap(gap = 0) { // gap: number, target과의 거리
     this.gap = gap;
   }
 
-  setOffset(offset) { // offset: number
+  setOffset(offset) { // offset: number, 올릴수록 화살표의 위치가 가운데로 움직인다.
     this.offset = offset;
   }
 
@@ -62,11 +69,37 @@ class Dropdown {
       }
     };
 
+    const createArrow = () => {
+      const arrow = document.createElement('div');
+      arrow.classList.add(styles[this.arrowDirection]);
+
+      if (this.direction === 'left') {
+        arrow.style.left = this.element.offsetWidth + 'px';
+        const pxForCenter = (this.target.offsetHeight - ARROW_LENGTH) * 0.5;
+        arrow.style.top = this.offset + pxForCenter + 'px';
+      } else if (this.direction === 'right') {
+        arrow.style.left = '-' + ARROW_HEIGHT + 'px';
+        const pxForCenter = (this.target.offsetHeight - ARROW_LENGTH) * 0.5;
+        arrow.style.top = this.offset + pxForCenter + 'px';
+      } else if (this.direction === 'up') {
+        arrow.style.bottom = '-' + ARROW_HEIGHT + 'px';
+        const pxForCenter = (this.target.offsetWidth - ARROW_LENGTH) * 0.5;
+        arrow.style.left = this.offset + pxForCenter + 'px';
+      } else {
+        arrow.style.top = '-' + ARROW_HEIGHT + 'px';
+        const pxForCenter = (this.target.offsetWidth - ARROW_LENGTH) * 0.5;
+        arrow.style.left = this.offset + pxForCenter + 'px';
+      }
+
+      this.element.append(arrow);
+    };
+
     this.element = document.createElement('ul');
     this.element.classList.add(styles.dropdown);
     
     createDropdownItems();
     this.target.append(this.element);
+    createArrow();
     setDropdownPosition();
   }
 
