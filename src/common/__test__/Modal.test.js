685c44866f14f3b979577dace3a5ab54e7b16eee
socratone <gim2origin@gmail.com>
2020-11-24 11:11:27 +0900

    DOM Text 추가

--- src/common/__test__/Modal.test.js
+++ src/common/__test__/Modal.test.js
@@ -1,3 +1,13 @@
+import {
+  getByLabelText,
+  getAllBy,
+  getByText,
+  getByTestId,
+  queryAllBy,
+  queryByTestId,
+  waitFor,
+} from '@testing-library/dom';
+import '@testing-library/jest-dom/extend-expect';
 import Modal from '../Modal.js';
 
 describe('Unit Test', () => {
@@ -54,8 +64,48 @@ describe('Unit Test', () => {
       expect(modal.executedFunction).toBe(func);
     });
   });
+});
 
-  describe('show 메소드', () => {
+describe('DOM Test', () => {
+  describe('새로운 재생 리스트를 만들 때 나오는 모달', () => {
+    let modal;
+    beforeEach(() => {
+      document.body.innerHTML = '';
+      modal = new Modal('medium');
+      modal.setTitle('Playlist');
+      modal.setDescription('새로 생성할 재생 리스트의 이름을 입력하세요.');
+      modal.setButtons('OK', 'Cancel');
+      modal.setInput('100%');
+      modal.setExecutedFunction(() => { 
+        // TODO: 재생 리스트 생성
+        console.log('다음 이름으로 재생 리스트를 생성합니다:', modal.input.value)
+      })
+      modal.show(); 
+    });
 
+    it('setTitle로 지정한 string이 표시 돼야 한다.', () => {
+      expect(document.body).toHaveTextContent('Playlist');
+    });
+    
+    it('setDescripttion으로 지정한 string이 표시 돼야 한다.', () => {
+      expect(document.body).toHaveTextContent('새로 생성할 재생 리스트의 이름을 입력하세요.');
+    });
+
+    it('setButtons로 지정한 string이 표시 돼야 한다.', () => {
+      expect(document.body).toHaveTextContent('OK');
+      expect(document.body).toHaveTextContent('Cancel');
+    });
+
+    it('입력 박스가 생성 돼야 한다.', () => {
+      expect(document.querySelector('.modalInput')).toBeTruthy();
+    });
+    
+    it('두 버튼이 생성 돼야 한다.', () => {
+      expect(document.querySelectorAll('button')[0]).toHaveTextContent('OK');
+      expect(document.querySelectorAll('button')[1]).toHaveTextContent('Cancel');
+      expect(document.querySelectorAll('button').length).toBe(2);
+    });
+
+    // TODO: 기능 동작 테스트
   });
 });
