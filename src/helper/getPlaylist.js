8e716e30de025f3a1a76fbbc66721af4c6e525ca
socratone <gim2origin@gmail.com>
2020-11-25 11:33:23 +0900

    에러 핸들링

--- src/helper/getPlaylist.js
+++ src/helper/getPlaylist.js
@@ -5,6 +5,8 @@ const url = 'http://localhost:4000/api/'
 const getPlaylist = () => {
   return fetch(url + 'playlist_video')
     .then(response => {
+      if (response.status !== 200) throw new Error(response.statusText);
+      console.log('response:', response)
       return response.json()
     })
     .catch(error => {
