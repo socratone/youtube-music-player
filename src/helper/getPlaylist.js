ac6e7b4a8bc55dd976a807d4cf2cb1114dfd72de
socratone <gim2origin@gmail.com>
2020-11-26 01:52:10 +0900

    getPlaylist에 axios 적용

--- src/helper/getPlaylist.js
+++ src/helper/getPlaylist.js
@@ -1,20 +1,20 @@
+import axios from 'axios';
 import { url } from '../config/config';
 import Modal from '../common/Modal';
 
-const getPlaylist = () => {
-  return fetch(url + '/api/playlist')
-    .then(response => {
-      if (response.status !== 200) throw new Error(response.statusText);
-      return response.json()
-    })
-    .catch(error => {
-      const modal = new Modal('medium');
-      modal.setTitle('오류');
-      modal.setDescription(`다음 오류가 발생했습니다.<br>${error}`);
-      modal.setButtons('확인');
-      modal.show();
-      return [];
-    });
+const getPlaylist = async () => {
+  try {
+    const response = await axios.get(`${url}/api/playlist`);
+    if (response.status !== 200) throw new Error(response.statusText);
+    return response.data;
+  } catch (error) {
+    const modal = new Modal('medium');
+    modal.setTitle('오류');
+    modal.setDescription(`다음 오류가 발생했습니다.<br>${error}`);
+    modal.setButtons('확인');
+    modal.show();
+    return [];
+  }
 };
 
 export default getPlaylist;
