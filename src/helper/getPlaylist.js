c6d30996c6926ed11d56df7eb124e2a31565e28d
socratone <gim2origin@gmail.com>
2020-11-25 11:19:57 +0900

    getPlaylist 요청 구현

---
+++ src/helper/getPlaylist.js
@@ -0,0 +1,20 @@
+import Modal from '../common/Modal';
+
+const url = 'http://localhost:4000/api/'
+
+const getPlaylist = () => {
+  return fetch(url + 'playlist_video')
+    .then(response => {
+      return response.json()
+    })
+    .catch(error => {
+      const modal = new Modal('medium');
+      modal.setTitle('오류');
+      modal.setDescription(`다음 오류가 발생했습니다.<br>${error}`);
+      modal.setButtons('확인');
+      modal.show();
+      return [];
+    });
+};
+
+export default getPlaylist;
